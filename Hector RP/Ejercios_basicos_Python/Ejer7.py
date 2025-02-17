d4d4b2c96322f2a55802181c639d28d1395b368b
Hector20062006 <hrp7511@gmail.com>
2025-02-17 12:27:38 +0100

    He subido todo por 5º vez

---
+++ Hector RP/Ejercios_basicos_Python/Ejer7.py
@@ -0,0 +1,4 @@
+minutos_totales = int(input("Ingrese la cantidad de minutos: "))
+horas = minutos_totales // 60
+minutos_restantes = minutos_totales % 60
+print(minutos_totales, "minutos son", horas, "horas y", minutos_restantes, "minutos.")
---
+++ Hector RP/Ejercios_basicos_Python/Ejr3.py
@@ -0,0 +1,6 @@
+import math
+
+cateto1 = float(input("Ingrese el primer cateto: "))
+cateto2 = float(input("Ingrese el segundo cateto: "))
+hipotenusa = math.sqrt(cateto1**2 + cateto2**2)
+print("La hipotenusa es:", hipotenusa)
