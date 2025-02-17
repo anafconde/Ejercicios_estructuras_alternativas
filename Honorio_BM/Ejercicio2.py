45d83c4086a79e84f583da801c43cc3398fb78eb
honorio <honoriobarranco@gmail.com>
2025-02-17 12:41:26 +0100

    Ejercicios impares

---
+++ Honorio_BM/Ejercicio2.py
@@ -0,0 +1,15 @@
+#Autor: Honorio
+#Version: 1.0
+#3.Escribe un programa que lea un número e indique si es par o impar.
+
+num=int(input("Introduzca un numero: "))
+
+
+if (num%2)==0:
+    print(f"El numero {num} es par")
+else:
+    print(f"El numero {num} es impar") 
+print("Fin")
+
+
+
