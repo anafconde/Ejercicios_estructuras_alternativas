afdfbd9af03a0286001d3485e3950c0744368d4a
Laura-Linares <lauralinarescontacto@gmail.com>
2025-02-17 18:35:22 +0100

    Exercise 11 modified and others ones added

--- LauraLL/Ejercicio11.py
+++ LauraLL/Ejercicio11.py
@@ -11,3 +11,17 @@
 A=float(input("Introduce el dato A de uno de los lados: "))
 B=float(input("Introduce el dato B de uno de los lados: "))
 C=float(input("Introduce el dato C de uno de los lados: "))
+
+#Compruebo el teorema de Pitágoras
+if C**2==((B**2)+(A**2)):
+    print("Se cumple el teorema de Pitágoras, así que es un TRIÁNGULO RECTÁNGULO")
+#Compruebo que todos los lados sean iguales
+elif C==A and A==B:
+    print("Es un TRIÁNGULO EQUILÁTERO")
+#Compruebo que dos lados sean iguales pero que el otro sea distinto, da igual el lado
+elif B==A or A==C or B==C:
+    if A!=C or A!=B or B!=C:
+        print("Es un TRIÁNGULO ISÓSCELES")
+#Ni no se cumple ninguna de las anteriores...
+else:
+    print("Es un TRIÁNGULO ESCALENO")
