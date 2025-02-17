53c6594a03fce72cfe521744ad16385231a3cbb3
Hector20062006 <hrp7511@gmail.com>
2025-02-17 12:19:47 +0100

    .

--- Hector Ramirez Pineda/Ejercios_basicos_Python/Ejer7.py
+++
@@ -1,4 +0,0 @@
-minutos_totales = int(input("Ingrese la cantidad de minutos: "))
-horas = minutos_totales // 60
-minutos_restantes = minutos_totales % 60
-print(minutos_totales, "minutos son", horas, "horas y", minutos_restantes, "minutos.")
--- Hector Ramirez Pineda/Ejercios_basicos_Python/Ejr2.py
+++
@@ -1,6 +0,0 @@
-base = float(input("Ingrese la base del rectángulo: "))
-altura = float(input("Ingrese la altura del rectángulo: "))
-area = base * altura
-perimetro = 2 * (base + altura)
-print("El área es:", area)
-print("El perímetro es:", perimetro)
