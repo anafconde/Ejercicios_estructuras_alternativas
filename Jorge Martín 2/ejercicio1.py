b7742822a004fb22fc65abd436cc1e25425b8fa4
jxrge06 <jorgemartinmartinez006@gmail.com>
2025-02-19 12:16:39 +0100

    Delete Jorge Martín 2 directory

--- Jorge Martín 2/ejercicio1.py
+++
@@ -1,9 +0,0 @@
-num1 = float(input("Introduce el primer número: "))
-num2 = float(input("Introduce el segundo número: "))
-
-if num1 > num2:
-    print("El primer número es mayor que el segundo.")
-else:
-    print("El primer número no es mayor que el segundo.")
-
-
--- Jorge Martín 2/ejercicio9.py
+++
@@ -1,6 +0,0 @@
-num1 = float(input("Introduce el primer número: "))
-num2 = float(input("Introduce el segundo número: "))
-num3 = float(input("Introduce el tercer número: "))
-
-numeros_ordenados = sorted([num1, num2, num3], reverse=True)
-print("Números ordenados de mayor a menor:", numeros_ordenados)
