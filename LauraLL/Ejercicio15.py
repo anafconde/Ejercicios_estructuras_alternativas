8648af02700520015343f32eafec3473903f5b3f
Laura-Linares <lauralinarescontacto@gmail.com>
2025-02-19 12:28:47 +0100

    Modified exercise 15

--- LauraLL/Ejercicio15.py
+++ LauraLL/Ejercicio15.py
@@ -4,32 +4,25 @@
 #El director de una escuela está organizando un viaje de estudios, y requiere determinar cuánto debe cobrar a cada alumno y cuánto debe pagar a la compañía de viajes por el servicio.
 #La forma de cobrar es la siguiente: 
 #si son 100 alumnos o más, el costo por cada alumno es de 65 euros; de 50 a 99 alumnos, el costo es de 70 euros, de 30 a 49, de 95 euros, 
-#y si son menos de 30. ????????
-#El costo de la renta del autobús es de 4000 euros, sin importar el número de alumnos. 
+#y si son menos de 30, el costo de la renta del autobús es de 4000 euros, sin importar el número de alumnos. 
 #Realice un algoritmo que permita determinar el pago a la compañía de autobuses y lo que debe pagar cada alumno por el viaje
 
 n_alum=int(input("Introduzca el número de alumnos que asistirán al viaje de estudios: "))
 
 if n_alum>=100:
-    costo=(n_alum*65)+4000
-    print(f"El coste de organización del viaje de estudios para {n_alum} alumnos + el autobús es de: {costo} €")
-    pago=costo/n_alum
-    print(f"Cada alumno deberá pagar un total de {pago} €")
+    costo=65
+    pago=costo*n_alum
+    print(f"Para un viaje de {n_alum}, cada alumno deberá pagar un total de {pago} €")
 elif n_alum>=50 and n_alum<=99:
-    costo=(n_alum*70)+4000
-    print(f"El coste de organización del viaje de estudios para {n_alum} alumnos + el autobús es de: {costo} €")
-    pago=costo/n_alum
-    print(f"Cada alumno deberá pagar un total de {pago} €")
+    costo=70
+    pago=costo*n_alum
+    print(f"Para un viaje de {n_alum}, cada alumno deberá pagar un total de {pago} €")
 elif n_alum>=30 and n_alum<=49:
-    costo=(n_alum*95)+4000
-    print(f"El coste de organización del viaje de estudios para {n_alum} alumnos + el autobús es de: {costo} €")
-    pago=costo/n_alum
-    print(f"Cada alumno deberá pagar un total de {pago} €")
+    costo=95
+    pago=costo*n_alum
+    print(f"Para un viaje de {n_alum}, cada alumno deberá pagar un total de {pago} €")
 elif n_alum>=0 and n_alum<=29:
-    #falta un dato en el enunciado, creo
-    costo=(n_alum)+4000
-    print(f"El coste de organización del viaje de estudios para {n_alum} alumnos + el autobús es de: {costo} €")
-    pago=costo/n_alum
-    print(f"Cada alumno deberá pagar un total de {pago} €")
+    pago=4000/n_alum
+    print(f"Para un viaje de {n_alum}, cada alumno deberá pagar un total de {pago} €")
 else:
     print("Valor introducido erróneo")
