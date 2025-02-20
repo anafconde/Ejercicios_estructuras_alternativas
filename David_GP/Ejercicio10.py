f852bd4f89dc0eda6108577eb589a122ee57bd68
David <david.gpinformatica@gmail.com>
2025-02-20 08:48:07 +0100

    Exercises updated

--- David_GP/Ejercicio10.py
+++ David_GP/Ejercicio10.py
@@ -11,3 +11,29 @@
 #interiores
 #concéntricas
 
+import math
+
+# Pedir al usuario que ingrese los datos de las circunferencias
+x1 = float(input("Introduce la coordenada x del centro de la primera circunferencia: "))
+y1 = float(input("Introduce la coordenada y del centro de la primera circunferencia: "))
+r1 = float(input("Introduce el radio de la primera circunferencia: "))
+x2 = float(input("Introduce la coordenada x del centro de la segunda circunferencia: "))
+y2 = float(input("Introduce la coordenada y del centro de la segunda circunferencia: "))
+r2 = float(input("Introduce el radio de la segunda circunferencia: "))
+
+# Calcular la distancia entre los centros de las circunferencias
+distancia_centros = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+# Clasificar la posición de las circunferencias
+if distancia_centros > r1 + r2:
+    print("Las circunferencias son exteriores.")
+elif distancia_centros == r1 + r2:
+    print("Las circunferencias son tangentes exteriores.")
+elif distancia_centros > abs(r1 - r2) and distancia_centros < r1 + r2:
+    print("Las circunferencias son secantes.")
+elif distancia_centros == abs(r1 - r2):
+    print("Las circunferencias son tangentes interiores.")
+elif distancia_centros < abs(r1 - r2):
+    print("Las circunferencias son interiores.")
+else:
+    print("Las circunferencias son concéntricas.")
---
+++ David_GP/Ejercicio13.py
@@ -0,0 +1,25 @@
+# Version 1.0
+# Autor David García Pérez
+
+#Ejercicio 13
+#Escribe un programa que pida una fecha (día, mes y año) y diga si es correcta.
+
+def fecha_valida(dia, mes, año):
+
+  if año < 1 or mes < 1 or mes > 12 or dia < 1:
+    return False
+  dias_en_mes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if año % 4 == 0 and (año % 100 != 0 or año % 400 == 0):
+    dias_en_mes[1] = 29
+  return dia <= dias_en_mes[mes - 1]
+
+# Solicitar la fecha al usuario
+dia = int(input("Ingresa el día: "))
+mes = int(input("Ingresa el mes: "))
+año = int(input("Ingresa el año: "))
+
+# Verificar si la fecha es correcta
+if fecha_valida(dia, mes, año):
+  print("La fecha es correcta.")
+else:
+  print("La fecha no es correcta.")
